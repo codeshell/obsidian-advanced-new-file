845c42cb963f35b3fb22c696d8614c2e9886410f
codeshell <git@tmw.app>
2026-06-01 04:43:45 +0200

    chore: prepare for submodule

--- scripts/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cp main.js .obsidian/plugins/obsidian-advanced-new-file/
-cp manifest.json .obsidian/plugins/obsidian-advanced-new-file/
-cp styles.css .obsidian/plugins/obsidian-advanced-new-file/
